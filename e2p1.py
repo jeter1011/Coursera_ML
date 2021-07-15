31c21e07d963599d23205ccfea09464bc97d0fc3
jeter1011 <erickagudelo123@gmail.com>
2021-07-15 15:26:31 -0400

    Update e2p1.py

--- e2p1.py
+++ e2p1.py
@@ -45,6 +45,10 @@ m, n = X.shape
 X = np.concatenate([np.ones((m, 1)), X], axis=1)
 
 
+def hypothesis(theta, X):
+    return sigmoid(np.dot(X, theta[None].T))
+
+
 def costFunction(theta, X, y):
     # Initialize some useful values
     m = y.size  # number of training examples
@@ -55,13 +59,31 @@ def costFunction(theta, X, y):
 
     # ====================== YOUR CODE HERE ======================
 
+    J = (1/m)*np.sum((np.dot(-y, np.log(hypothesis(theta, X)))) - (np.dot((1 - y),(np.log(1-hypothesis(theta, X))))))
+    grad = (1 / m) * np.dot((hypothesis(theta, X) - y), X)
     # =============================================================
     return J, grad
 
 
 theta = np.zeros(n + 1)
+initial_theta = np.zeros(n+1)
+cost, grad = costFunction(theta, X, y)
 
-b = (np.dot(X, theta[None].T))
+print('Cost at initial theta (zeros): {:.3f}'.format(cost))
+print('Expected cost (approx): 0.693\n')
 
+print('Gradient at initial theta (zeros):')
+print('\t[{:.4f}, {:.4f}, {:.4f}]'.format(*grad))
+print('Expected gradients (approx):\n\t[-0.1000, -12.0092, -11.2628]\n')
 
+# Compute and display cost and gradient with non-zero theta
+test_theta = np.array([-24, 0.2, 0.2])
+cost, grad = costFunction(test_theta, X, y)
+
+print('Cost at test theta: {:.3f}'.format(cost))
+print('Expected cost (approx): 0.218\n')
+
+print('Gradient at test theta:')
+print('\t[{:.3f}, {:.3f}, {:.3f}]'.format(*grad))
+print('Expected gradients (approx):\n\t[0.043, 2.566, 2.647]')
 
