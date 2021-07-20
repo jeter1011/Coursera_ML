e0bf7c6c628ead06476554a5c2ff7938adf80f98
jeter1011 <erickagudelo123@gmail.com>
2021-07-20 14:17:45 -0400

    Update e2p1.py

--- e2p1.py
+++ e2p1.py
@@ -46,7 +46,7 @@ X = np.concatenate([np.ones((m, 1)), X], axis=1)
 
 
 def hypothesis(theta, X):
-    return sigmoid(np.dot(X, theta[None].T))
+    return sigmoid(np.dot(X, theta.T))
 
 
 def costFunction(theta, X, y):
@@ -73,17 +73,18 @@ print('Cost at initial theta (zeros): {:.3f}'.format(cost))
 print('Expected cost (approx): 0.693\n')
 
 print('Gradient at initial theta (zeros):')
-print('\t[{:.4f}, {:.4f}, {:.4f}]'.format(*grad))
+#print('\t[{:.4f}, {:.4f}, {:.4f}]'.format(*grad))
 print('Expected gradients (approx):\n\t[-0.1000, -12.0092, -11.2628]\n')
 
 # Compute and display cost and gradient with non-zero theta
-test_theta = np.array([-24, 0.2, 0.2])
-cost, grad = costFunction(test_theta, X, y)
+#test_theta = np.array([-24, 0.2, 0.2])
+#cost, grad = costFunction(test_theta, X, y)
 
-print('Cost at test theta: {:.3f}'.format(cost))
-print('Expected cost (approx): 0.218\n')
+#print('Cost at test theta: {:.3f}'.format(cost))
+#print('Expected cost (approx): 0.218\n')
 
-print('Gradient at test theta:')
-print('\t[{:.3f}, {:.3f}, {:.3f}]'.format(*grad))
-print('Expected gradients (approx):\n\t[0.043, 2.566, 2.647]')
+#print('Gradient at test theta:')
+#print('\t[{:.3f}, {:.3f}, {:.3f}]'.format(*grad))
+#print('Expected gradients (approx):\n\t[0.043, 2.566, 2.647]')
+
 
